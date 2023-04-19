82e3486be528bc4fbd1bb4cb6451f7c5ab80378b
Andrés <andrescn1007@gmail.com>
2023-04-18 20:18:42 -0400

    feat: adding pagination and tags

--- components/sliders/EventSlider/EventCard.tsx
+++ components/sliders/EventSlider/EventCard.tsx
@@ -1,5 +1,5 @@
-import { FC, MouseEventHandler, useState } from 'react';
 import Link from 'next/link';
+import { FC, MouseEventHandler, useState } from 'react';
 import { Heart } from '../../assets/svg/Heart';
 import { User } from '../../assets/svg/User';
 
@@ -18,7 +18,6 @@ const EventCard: FC<EventCardProps> = ({
   url,
   votos,
 }) => {
-
   const [isActive, setIsActive] = useState<boolean>(false);
 
   const handleClick: MouseEventHandler<HTMLSpanElement> = (event) => {
@@ -28,16 +27,16 @@ const EventCard: FC<EventCardProps> = ({
 
   return (
     <Link href={url}>
-      <div className="relative rounded-xl overflow-hidden max-w-[298px] h-[454px] bg-white border-solid border-black card-shadow mb-1">
+      <div className="relative rounded-xl overflow-hidden min-w-[298px] h-[454px] bg-white border-solid border-black card-shadow mb-1">
         <img
           src={imageUrl}
           alt={name}
           className="w-full h-[239px] object-cover"
         />
         <div className="absolute top-[205px] right-4">
-         <span onClick={handleClick}>
-         <Heart isActive={isActive}  />
-          </span> 
+          <span onClick={handleClick}>
+            <Heart isActive={isActive} />
+          </span>
         </div>
         <div className="w-full px-4 py-3 bg-white bg-opacity-70">
           <h3 className="app-title-3">{name}</h3>
@@ -46,7 +45,7 @@ const EventCard: FC<EventCardProps> = ({
               {description}
             </p>
           </div>
-          <p className="mt-3 text-app-blue font-medium text-sm">ladygaga.com</p>
+          <p className="mt-3 text-app-blue font-medium text-sm">{url}</p>
           <div className="flex gap-2 mt-2">
             <User />
             <p className="app-text-2 font-semibold mt-[4.4px]">{votos} votos</p>
